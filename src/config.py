3f6bbb9c140f67b6aefb925d04e2a650d6a7d6db
RushHp <namerandomhp@gmail.com>
2025-09-13 05:54:45 +0300

    Добавил все файлы проекта

---
+++ src/config.py
@@ -0,0 +1,8 @@
+import os  # импортируем модуль для работы с переменными окружения
+
+# Telegram токен (можно хранить в переменной окружения или прямо прописать)
+BOT_TOKEN = os.getenv("BOT_TOKEN", "ВАШ_ТОКЕН_ЗДЕСЬ")
+
+# Пути к данным и модели
+DATA_PATH = "data/sentiment_dataset.csv"  # путь к CSV с дата сетом
+MODEL_PATH = "src/sentiment_model.pkl"    # путь для сохранения модели
